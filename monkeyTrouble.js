9ef9055bc2ee4ada5fee27b541d78a4cc6fc520d
charmii03 <charmigangani46@gmail.com>
2022-07-05 15:38:56 +0530

    js

---
+++ monkeyTrouble.js
@@ -0,0 +1,11 @@
+//monkeyTrouble js
+function monkeyTrouble(aSmile, bSmile){
+  if (aSmile && bSmile) {
+    return true;
+  }
+  if (!aSmile && !bSmile) 
+  {
+    return true;
+  }
+  return false;
+}
